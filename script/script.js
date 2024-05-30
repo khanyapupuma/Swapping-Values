765a2da0dd746c0e0b57f6fdf25ac18e12d83ea7
khanyapupuma <khalilpupuma@gmail.com>
2024-05-30 10:14:29 +0200

    swap

---
+++ script/script.js
@@ -0,0 +1,28 @@
+let firstNumber = 17;
+let secondNumber = 15;
+
+// First Method : Using a third variable(Temporary variable)
+
+// let thirdNumber = firstNumber;
+// firstNumber = secondNumber;
+// secondNumber = thirdNumber;
+
+// console.log(firstNumber)
+// console.log(secondNumber)
+
+// Second Method: Destructuring
+
+// [firstNumber, secondNumber] = [secondNumber, firstNumber]
+
+// console.log(firstNumber)
+// console.log(secondNumber)
+
+// Third Method: Using Arithmetic Operators
+
+// firstNumber = firstNumber + secondNumber
+// secondNumber = firstNumber - secondNumber
+// firstNumber =  firstNumber - secondNumber
+
+// console.log(firstNumber);
+// console.log(secondNumber);
+
